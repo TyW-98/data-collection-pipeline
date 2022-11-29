8bc6a19bb2b702bbb89b03787b5d58f173e51757
wey1998 <yitwey1998@hotmail.com>
2022-11-30 02:35:24 +0800

    Scraper_class.py to scrape agoda

--- scraper_class.py
+++ scraper_class.py
@@ -1,6 +1,32 @@
 import requests 
 import time
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
 
+driver = webdriver.Chrome()
+driver.get("https://www.agoda.com/?cid=1844104")
+time.sleep(2)
 
-main_page = requests.get("https://www.agoda.com/?cid=1844104")
+destination = "Penang"
+search_bar = driver.find_element(by = By.XPATH, value = '//*[@class = "SearchBoxTextEditor SearchBoxTextEditor--autocomplete"]')
+search_bar.send_keys(destination)
+driver.find_element(by = By.XPATH, value = '//button[@class = "Buttonstyled__ButtonStyled-sc-5gjk6l-0 hvHHEO Box-sc-kv6pi1-0 fDMIuA"]').click()
+time.sleep(15)
+
+hotel_list = [] 
+
+all_hotel = driver.find_elements(by = By.XPATH, value = '//*[@class = "PropertyCard__Link"]')
+
+for hotel in all_hotel:
+    hotel_link = hotel.get_attribute("href")
+    hotel_list.append(hotel_link)
+    
+for hotel in hotel_list:
+    driver.get(hotel)
+    hotel_name = driver.find_element(by = By.XPATH, value = '//*[@data-selenium = "hotel-header-name"]').text
+    print(hotel_name)
+    time.sleep(5)
+    
+print(hotel_list)
